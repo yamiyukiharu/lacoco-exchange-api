3468b14b3530dda129772826713b44d1937d076f
Hao Ye <haoye94@hotmail.com>
2023-04-13 14:41:21 +0800

    Update image urls and cors

--- src/token/services/Token.service.ts
+++ src/token/services/Token.service.ts
@@ -8,38 +8,38 @@ import { GetTokenPricesResponse, TokenInfoType } from '../types';
 @Injectable()
 export class TokenService {
   private readonly PRICE_CACHE_KEY = 'tokenPrices';
-  private readonly PRICE_CACHE_TTL = 10000;
+  private readonly PRICE_CACHE_TTL = 20000;
 
   TOKENS: TokenInfoType = {
     [Token.BTC]: {
       symbol: 'BTC',
       name: 'Bitcoin',
       decimals: 6,
-      icon: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+      icon: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579',
     },
     [Token.ETH]: {
       symbol: 'ETH',
       name: 'Ethereum',
       decimals: 6,
-      icon: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+      icon: 'https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880',
     },
     [Token.DFI]: {
       symbol: 'DFI',
       name: 'DeFiChain',
       decimals: 6,
-      icon: 'https://assets.coingecko.com/coins/images/14349/large/defichain.png?1615897939',
+      icon: 'https://assets.coingecko.com/coins/images/11757/small/symbol-defi-blockchain_200.png?1597306538',
     },
     [Token.USDT]: {
       symbol: 'USDT',
       name: 'Tether',
       decimals: 2,
-      icon: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      icon: 'https://assets.coingecko.com/coins/images/325/small/Tether.png?1668148663',
     },
     [Token.DOGE]: {
       symbol: 'DOGE',
       name: 'Dogecoin',
       decimals: 6,
-      icon: 'https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256',
+      icon: 'https://assets.coingecko.com/coins/images/5/small/dogecoin.png?1547792256',
     },
   };
 
